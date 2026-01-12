cb6470f72af648a7ac576f8cb1fd555075285a1e
JSunicorn <11246003@ntub.edu.tw>
2026-01-12 16:46:43 +0800

    測試4-2

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,9 @@ public class Main {
             new Shop(
                 "福勝亭",
                 new Product[]{
-                    new Product("經典豬排定食套餐", 292),
-                    new Product("開運豬排定食", 247),
-                    new Product("香酥炸腰內肉定食", 247)
+                    new Product("經典豬排定食套餐", 292, 0.9),
+                    new Product("開運豬排定食", 247, 0.75),
+                    new Product("香酥炸腰內肉定食", 247, 0.95)
                 }
             ),
             new Shop(
@@ -19,11 +19,11 @@ public class Main {
             new Shop(
                 "阜杭豆漿",
                 new Product[]{
-                    new Product("厚燒餅", 40),
-                    new Product("厚餅夾蛋", 55),
-                    new Product("豆漿", 45),
-                    new Product("糙米漿", 45),
-                    new Product("鹹豆漿", 60)
+                    new Product("厚燒餅", 40, 0.8),
+                    new Product("厚餅夾蛋", 55, 0.8),
+                    new Product("豆漿", 45, 0.5),
+                    new Product("糙米漿", 45, 0.5),
+                    new Product("鹹豆漿", 60, 0.8)
                 }
             )
         };
@@ -33,7 +33,7 @@ public class Main {
         for (Shop shop : shops) {
             System.out.println(shop.name + "：");
             for (Product product : shop.products) {
-                System.out.println("\t" + product.name + "\t-\t$" + String.format("%,d", product.price) + "元");
+                System.out.println("\t" + product.name + "\t-\t$" + String.format("%,.0f", product.calculateDiscountPrice()) + "元");
             }
         }
     }
@@ -52,9 +52,19 @@ class Shop {
 class Product {
     public final String name;
     public final int price;
+    public final double discount;
 
     public Product(String name, int price) {
+        this(name, price, 1);
+    }
+
+    public Product(String name, int price, double discount) {
         this.name = name;
         this.price = price;
+        this.discount = discount;
+    }
+
+    public double calculateDiscountPrice() {
+        return price * discount;
     }
 }
